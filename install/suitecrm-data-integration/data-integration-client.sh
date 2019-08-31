197980beda2f3679c5d8a61fee186474d0ac8b55
Harris Ward <harris.ward@ivy-is.co.uk>
2019-08-31 17:06:53 +0100

    Added data integration startup script and fixed DDLs to drop tables if they already exist

---
+++ install/suitecrm-data-integration/data-integration-client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+today=`date +%Y-%m-%d.%H:%M:%S`
+
+echo ""
+echo " The data extraction process is running..."
+echo " You can see the output in the logs directory"
+
+cd scripts/
+./spoon.sh
