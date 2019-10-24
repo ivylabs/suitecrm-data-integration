c5e874997731efac83bbd282a3bf6b40131094c2
Harris Ward <harris.ward@ivy-is.co.uk>
2019-10-24 12:50:51 +0000

    Add missing S_LEADS_AUDIT DDL

---
+++ install/suitecrm-data-integration/setup/DDL/S_LEADS_AUDIT.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `S_LEADS_AUDIT`;
+CREATE TABLE `S_LEADS_AUDIT` (
+  `id` varchar(36) DEFAULT NULL,
+  `parent_id` varchar(36) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `created_by` varchar(36) DEFAULT NULL,
+  `field_name` varchar(100) DEFAULT NULL,
+  `data_type` varchar(100) DEFAULT NULL,
+  `before_value_string` varchar(255) DEFAULT NULL,
+  `after_value_string` varchar(255) DEFAULT NULL,
+  `before_value_text` varchar(255) DEFAULT NULL,
+  `after_value_text` varchar(255) DEFAULT NULL
+)
